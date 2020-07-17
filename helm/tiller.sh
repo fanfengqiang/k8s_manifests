d08391ac3101e7414f12988ef680ad5c8060b856
k8s <1415228958@qq.com>
2020-07-17 14:45:52 +0800

    update

--- helm/tiller.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl create serviceaccount --namespace kube-system tiller
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-helm init
-kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
